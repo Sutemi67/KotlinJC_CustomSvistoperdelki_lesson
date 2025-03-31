3be8a5ad0db1ceb3c8bdf7cab1b4ee061a61d031
SergeyBoykov <sutemi67@gmail.com>
2025-03-31 09:56:15 +0300

    Initial commit

---
+++ app/src/main/java/acr/appcradle/kotlinjc_customsvistoperdelki_lesson/MainActivity.kt
@@ -0,0 +1,47 @@
+package acr.appcradle.kotlinjc_customsvistoperdelki_lesson
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import acr.appcradle.kotlinjc_customsvistoperdelki_lesson.ui.theme.KotlinJC_CustomSvistoperdelki_lessonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinJC_CustomSvistoperdelki_lessonTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinJC_CustomSvistoperdelki_lessonTheme {
+        Greeting("Android")
+    }
+}
